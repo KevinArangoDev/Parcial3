0d272fd40104c9e5581ba5cedf1f20cc0a44d3d7
Kevin Velasquez <Keviin.stiiven99@gmail.com>
2023-05-26 10:09:29 -0500

    Solo puse el boton de volver

--- DiplomadoParcial3/src/co/edu/uniquindio/parcial3/controller/AgregarEstudianteController.java
+++ DiplomadoParcial3/src/co/edu/uniquindio/parcial3/controller/AgregarEstudianteController.java
@@ -7,10 +7,14 @@ import co.edu.uniquindio.parcial3.exception.ClasePrincipalException;
 import co.edu.uniquindio.parcial3.model.Estudiante;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class AgregarEstudianteController implements Initializable {
 
@@ -45,6 +49,9 @@ public class AgregarEstudianteController implements Initializable {
 
 	    @FXML
 	    private TextField txtGenero;
+
+	    @FXML
+	    private Button btnVolver;
 
 	    @FXML
 	    private Button btnAgregarEstudiante1;
@@ -97,7 +104,28 @@ public class AgregarEstudianteController implements Initializable {
 		}
 
 
+	    void volverInicio(ActionEvent event){
 
+	    	try {
+				FXMLLoader loader = new FXMLLoader(
+						getClass().getResource("../view/InicioView.fxml"));
+				Parent root = loader.load();
+
+//				PrincipalController controlador = loader.getController();
+
+				Scene scene = new Scene(root);
+				Stage stage = new Stage();
+				stage.setTitle("Diplomado");
+//				stage.getIcons().add(new Image(getClass().getResourceAsStream("/resources/Logo Subasta.png")));
+				stage.setScene(scene);
+				stage.show();
+
+				Stage myStage = (Stage) this.btnVolver.getScene().getWindow();
+				myStage.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+	    }
 
 	}
 
